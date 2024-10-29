5fcb9692d40066b26f44d5d1c3f1b51a8dffd85e
RaphaelRato <raphaelrato2002@gmail.com>
2024-10-29 16:06:37 +0200

    Added utilities classes and basic implementation
    Added .h and .cpp files and classes for:
    -UtilitiesDecorator
    -WasteManagementDecorator
    -SewageManagementDecorator
    -WaterSupplyDeocrator
    -PowerPlantDecorator
    
    Mostly basic implementation

---
+++ src/PowerPlantDecorator.h
@@ -0,0 +1,11 @@
+#ifndef POWERPLANTDECORATOR_H
+#define POWERPLANTDECORATOR_H
+
+#include "UtilitiesDecorator.h"
+
+class PowerPlantDecorator : public UtilitiesDecorator{
+    public:
+        PowerPlantDecorator();
+};
+
+#endif
