52c1e97807699c34b5576cb2b4fa565f74830d68
Tom Schulz <91066892+tomschulz02@users.noreply.github.com>
2024-10-30 09:58:46 +0200

    Update City.h includes

--- src/City.h
+++ src/City.h
@@ -2,6 +2,9 @@
 #define CITY_H
 
 #include "Satisfaction/SatisfactionState.h"
+#include "building/BuildingComponent.h"
+#include "ModeOfTransport.h"
+#include "UtilityFacade.h"
 
 class City {
     private:
