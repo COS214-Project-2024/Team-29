7cf80867b2dfed182c5912b6d01423cd5c83566e
Tom Schulz <schulztom02@gmail.com>
2024-10-31 09:37:35 +0200

    Added facade interface class

---
+++ src/Interface.h
@@ -0,0 +1,24 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "Government.h"
+#include "City.h"
+
+class Interface {
+    private:
+        Government* gov;
+    
+    public:
+        /// @brief default constructor for Interface
+        Interface();
+        /// @brief calls the construct command from government
+        void build();
+        /// @brief used to generate a random new policy in government
+        void policy();
+        /// @brief changes the tax rate base on user input
+        /// @param change 
+        //void taxes(double change);
+        ~Interface();
+};
+
+#endif
