ae8e13648879784039c1b4a34e935eeebf1f1fc1
EsterhuizenRuan <ruan.esterhuizen123@gmail.com>
2024-11-01 07:45:15 +0200

    added memento on city side

--- src/managers/City.cpp
+++ src/managers/City.cpp
@@ -17,6 +17,27 @@ void City::addTransport(ModeOfTransport *transport)
     this->modesOfTransport.push_back(transport);
 }
 
+CityMemento City::saveGame()
+{
+    //corresponds to Memento::createMemento()
+    CityMemento* save = new CityMemento();
+    save->setSatisfaction(this->satisfaction);
+    save->setBuildings(this->buildings);
+    save->setModesOfTransport(this->modesOfTransport);
+    // save->setGovernment(this->government);
+
+    //any attributes that get added to city must be here
+    return save;
+}
+
+City::loadGame(CityMemento save)
+{
+    //corresponds to Memento::setMemento(memento)
+    this->satisfaction = save->getSatisfaction();
+    this->buildings = save->getBuildings();
+    this->modesOfTransport = save->getModesOfTransport();
+}
+
 City::~City(){
     // delete other class variables here
     delete satisfaction;
