0c3920e7fef5d0423c977ad034ce9eaa287bc536
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-02 11:40:09 +0200

    Utility implementation changes
    -Utility Functions changed and updated
    -Utility demand vars removed
    -City demand vars added and new func comments added

--- src/utility/UtilityFacade.cpp
+++ src/utility/UtilityFacade.cpp
@@ -8,35 +8,27 @@ UtilityFacade::UtilityFacade()
     waterSupply = new WaterSupplyDecorator(); 
 }
 
-void UtilityFacade::calculateUtilityDemands(int numBuilding, int population)
+void UtilityFacade::checkAndUpgradeUtilities(int demand)
 {
-    totalPowerDemand = (numBuilding*10) + (population); 
-    totalSewageDemand = (numBuilding*12) + (population);
-    totalWasteDemand = (numBuilding*15) + (population);
-    totalWaterDemand = (numBuilding*15) + (population);
-}
-
-void UtilityFacade::checkAndUpgradeUtilities()
-{
-    if(totalPowerDemand > getPowerCapacity())
+    if(powerPlant->isEfficient(demand))
     {
         powerPlant->upgardeLevel();
         std::cout<<"Power Plant has been upgraded since demand was to high\n";
     }
 
-    if(totalSewageDemand > getSewageCapacity())
+    if(powerPlant->isEfficient(demand))
     {
         sewageManagement->upgardeLevel();
         std::cout<<"Sewage Management has been upgraded since demand was to high\n";
     }
 
-    if(totalWasteDemand > getWasteCapacity())
+    if(powerPlant->isEfficient(demand))
     {
         wasteManagement->upgardeLevel();
         std::cout<<"Waste Management has been upgraded since demand was to high\n";
     }
 
-    if(totalWaterDemand > getWaterCapacity())
+    if(powerPlant->isEfficient(demand))
     {
         waterSupply->upgardeLevel();
         std::cout<<"Water Supply has been upgraded since demand was to high\n";
@@ -45,13 +37,6 @@ void UtilityFacade::checkAndUpgradeUtilities()
 
 void UtilityFacade::printResourceDistribution()
 {
-    std::cout<<"Current Utilities Demands"<<std::endl;
-    std::cout<<"--------------------------"<<std::endl;
-    std::cout<<"Power Demand: "<<totalPowerDemand<<std::endl;
-    std::cout<<"Sewage Demand: "<<totalSewageDemand<<std::endl;
-    std::cout<<"Waste Demand: "<<totalWasteDemand<<std::endl;
-    std::cout<<"Water Demand: "<<totalWaterDemand<<std::endl;
-
     std::cout<<"Current Utilities Output Capacities"<<std::endl;
     std::cout<<"--------------------------"<<std::endl;
     std::cout<<"Power Capacity: "<<getPowerCapacity()<<std::endl;
@@ -66,26 +51,6 @@ UtilityFacade::~UtilityFacade()
     delete sewageManagement;
     delete waterSupply;
     delete wasteManagement;
-}
-
-int UtilityFacade::getTotalPowerDemand()
-{
-    return totalPowerDemand;
-}
-
-int UtilityFacade::getTotalWaterDemand()
-{
-    return totalWaterDemand;
-}
-
-int UtilityFacade::getTotalWasteDemand()
-{
-    return totalWasteDemand;
-}
-
-int UtilityFacade::getTotalSewageDemand()
-{
-    return totalSewageDemand;
 }
 
 int UtilityFacade::getPowerCapacity()
