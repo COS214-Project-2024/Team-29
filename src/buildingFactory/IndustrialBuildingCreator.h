9eebefcb2cdacc8a443a274c938f1e82f43239e0
Tom Schulz <schulztom02@gmail.com>
2024-11-02 19:29:47 +0200

    Updated building creator classes

--- src/buildingFactory/IndustrialBuildingCreator.h
+++ src/buildingFactory/IndustrialBuildingCreator.h
@@ -9,7 +9,7 @@
 class IndustrialBuildingCreator : public BuildingCreator
 {
     public:
-        Building* createBuilding(std::string buildType);
+        Building* createBuilding(int type);
 };
 
 #endif
