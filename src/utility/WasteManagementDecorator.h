22caf77ccc4076473c728f3795cd8327662d8c2a
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-02 19:29:12 +0200

    Doxygen Comments for Utilities

--- src/utility/WasteManagementDecorator.h
+++ src/utility/WasteManagementDecorator.h
@@ -3,8 +3,20 @@
 
 #include "UtilitiesDecorator.h"
 
-class WasteManagementDecorator : public UtilitiesDecorator{
+/**
+ * @class WasteManagementDecorator
+ * @brief A decorator class for the waste management utility.
+ *
+ * This class extends the `UtilitiesDecorator`
+ */
+class WasteManagementDecorator : public UtilitiesDecorator {
     public:
+        /**
+         * @brief Default constructor for WasteManagementDecorator.
+         *
+         * Initializes the waste management utility and initialises capacity to 120,
+         * building upon the base functionality provided by `UtilitiesDecorator`.
+         */
         WasteManagementDecorator();
 };
 
--- src/utility/WaterSupplyDecorator.h
+++ src/utility/WaterSupplyDecorator.h
@@ -3,8 +3,20 @@
 
 #include "UtilitiesDecorator.h"
 
+/**
+ * @class WaterSupplyDecorator
+ * @brief A decorator class for the water supply utility.
+ *
+ * This class extends the `UtilitiesDecorator`
+ */
 class WaterSupplyDecorator : public UtilitiesDecorator{
     public:
+        /**
+         * @brief Default constructor for WaterSupplyDecorator.
+         *
+         * Initializes the water supply utility and initialises capacity to 200,
+         * building upon the base functionality provided by `UtilitiesDecorator`.
+         */
         WaterSupplyDecorator();
 };
 
