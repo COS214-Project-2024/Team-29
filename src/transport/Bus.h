e8b2cf32ec0bdbc95a450b8f65701867b4aedf5c
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-04 01:53:47 +0200

    Transport Dox Comments

--- src/transport/Bus.h
+++ src/transport/Bus.h
@@ -6,27 +6,50 @@ using namespace std;
 
 #include "ModeOfTransport.h"
 
+/**
+ * @class Bus
+ * @brief Represents an bus with specified cost, capacity, and transport type.
+ *
+ * The Bus class is a specialised mode of transport within a city,
+ * providing an implementation specific to buses, including attributes for cost, type, and capacity.
+ */
 class Bus: public ModeOfTransport {
+    private:
+        float cost;     /**< The cost associated with the bus. */
+        string name;    /**< The name of the bus. */
 
     public:
-        /// @brief Creates a new bus for the city. 
-        ///
-        ///- Cost: R 100 000. 
-        ///
-        ///- Type: PublicTransport 
-        ///
-        ///- Capacity: 5 000
-        /// @param name the name of the bus
-        /// @throws runtime_error if there is not enough budget for the bus
+        /**
+         * @brief Creates a new bus for the city with specified name.
+         * 
+         * - Cost: R 100,000.
+         * - Type: PublicTransport.
+         * - Capacity: 5,000
+         * 
+         * @param name The name of the bus.
+         * @throws runtime_error if there is not enough budget to build the buses.
+         */
         Bus(string name, float& budget);
+        /**
+         * @brief Destructor for Bus, decrements publicTransportCapacity by its own capacity.
+         */
         ~Bus();
+        /**
+         * @brief Gets the type of transport associated with the bus.
+         * @return A string representing the transport type, ergo - "PublicTransport".
+         */
         string getTransportType();
+        /**
+         * @brief Gets the name of the bus.
+         * @return The name of the bus as a string.
+         */
         string getName();
+        /**
+         * @brief Gets the specific transport type label of the bus.
+         * @return A string representing the type  of transport, "Bus".
+         */
         string getType();
 
-    private:
-        float cost;
-        string name;
 };
 
 #endif
--- src/transport/Train.h
+++ src/transport/Train.h
@@ -6,27 +6,50 @@ using namespace std;
 
 #include "ModeOfTransport.h"
 
+/**
+ * @class Train
+ * @brief Represents an train with specified cost, capacity, and transport type.
+ *
+ * The Train class is a specialised mode of transport within a city,
+ * providing an implementation specific to airports, including attributes for cost, type, and capacity.
+ */
 class Train: public ModeOfTransport {
+    private:
+        float cost;     /**< The cost associated with the train. */
+        string name;    /**< The name of the train. */
 
     public:
-        /// @brief Creates a new train for the city. 
-        ///
-        ///- Cost: R 500 000. 
-        ///
-        ///- Type: GoodsTransport 
-        ///
-        ///- Capacity: 100 000
-        /// @param name the name of the train
-        /// @throws runtime_error if there is not enough budget to build the train
+        /**
+         * @brief Creates a new train for the city with specified name.
+         * 
+         * - Cost: R 500,000.
+         * - Type: GoodsTransport.
+         * - Capacity: 100,000
+         * 
+         * @param name The name of the train.
+         * @throws runtime_error if there is not enough budget to build the trains.
+         */
         Train(string name, float& budget);
+        /**
+         * @brief Destructor for Train, decrements goodTransportCapacity by its own capacity.
+         */
         ~Train();
+        /**
+         * @brief Gets the type of transport associated with the train.
+         * @return A string representing the transport type, ergo - "GoodsTransport".
+         */
         string getTransportType();
+        /**
+         * @brief Gets the name of the train.
+         * @return The name of the train as a string.
+         */
         string getName();
+        /**
+         * @brief Gets the specific transport type label of the train.
+         * @return A string representing the type  of transport, "Train".
+         */
         string getType();
         
-    private:
-        float cost;
-        string name;
 };
 
 #endif
