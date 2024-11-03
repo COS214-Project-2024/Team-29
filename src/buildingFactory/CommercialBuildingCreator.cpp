c401458966ab8cb8022bef4a435cf29ddcbecb45
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-04 01:52:25 +0200

    Factory creators Dox Comments

--- src/buildingFactory/CommercialBuildingCreator.cpp
+++ src/buildingFactory/CommercialBuildingCreator.cpp
@@ -5,19 +5,21 @@
 #include "../buildingComposite/commercial/Shop.h"
 #include "../buildingComposite/commercial/Office.h"
 
-// Input: type (int)
-//  1 - Office
-//  2 - Shop
-//  3 - Mall
+/*
+Input: type (int)
+    1 - Office
+    2 - Shop
+    3 - Mall
+*/
 Building* CommercialBuildingCreator::createBuilding(int bName) {
     switch (bName)
     {
         case 1:
             return new Office(500, 500000, 50);
-        break;
+            break;
         case 2:
             return new Shop(200, 150000, 10);
-        break;
+            break;
         case 3:
             return new Mall(1000, 2000000, 200);
             break;
