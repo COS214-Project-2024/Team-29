016010e9fbf24e2885527a0c807c7195bb8d14f7
Tom Schulz <schulztom02@gmail.com>
2024-11-03 14:00:49 +0200

    Added game loop to interface

--- src/managers/Interface.cpp
+++ src/managers/Interface.cpp
@@ -1,30 +1,51 @@
 #include "Interface.h"
+#include <iostream>
 
 Interface::Interface(){
-    this->gov = Government::instance();
+    this->c = new City();
+    this->cycle = 0;
 }
 
 Interface::~Interface(){
-    //delete this->gov;
+    delete this->c;
 }
 
-// creates a new building depending on the user's choice
-// uses the construct command from Government
-// assumes there is a construct method for each building type
-void Interface::build(){
-    // choice: 1 - Residential
-    //         2 - Commercial
-    //         3 - Industrial
-    //         4 - Landmark
-    
+void Interface::startSimulation(){
+    // initial value of -1 so that nothing is done until the user makes a choice
+    int choice = -1;
+    std::cout<<"Welcome to our city building simulator"<<std::endl;
+
+    // main game loop
+    // increments number of cycles
+    do {
+        std::cout<<"Cycle "<<this->cycle<<"\n"
+                 <<"==============\n";
+
+        // choice loop
+        // allows user to make multiple choices per cycle until they quit or go ot the next cycle
+        while (handleChoice(choice)){
+            std::cout<<"Choose an option to manage your city:\n"
+                 <<"\t1. Add building\n"
+                 <<"\t2. Implement a new policy\n"
+                 <<"\t3. Next cycle\n"
+                 <<"\t0. Quit simulation\n"
+                 <<"====================\n\n";
+            std::cin>>choice;
+        }
+
+        cycle++;
+
+    } while (choice != 0 && this->c->getPopulation() > 0);
 }
 
-// assumes there is a generate policy method that randomly generates either a positive or negative policy
-void Interface::policy(){
-
+// returns true if the simulation should advance to the next cycle
+bool Interface::handleChoice(int choice){
+    if (choice == 3 || choice == 0){
+        return true;
+    } else if (choice == 1){
+        // tell Building Manager to add a building
+    } else if (choice == 2){
+        this->c->implementPolicy();
+    }
+    return false;
 }
-
-// assumes there is a adjust tax method that changes the tax rate according to user input
-/* void Interface::taxes(double change){
-    gov->getResources().adjustTaxes(change);
-} */
