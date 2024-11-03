c0cc8cee1fd8df53dcbe7065df14a767d80f68ea
Tom Schulz <schulztom02@gmail.com>
2024-11-03 19:03:46 +0200

    Added building choice

--- src/managers/Interface.cpp
+++ src/managers/Interface.cpp
@@ -56,9 +56,38 @@ bool Interface::handleChoice(int choice){
     if (choice == 3 || choice == 0){
         return true;
     } else if (choice == 1){
+        int type;
+        std::cout<<"Select which building you would like to add:\n"
+            <<"------------------------------------\n"
+            <<"Residential Buildings:\n"
+            <<"\t1. Townhouse\n\t2. Estate\n\t3. House\n"
+            <<"Commercial Buildings:\n"
+            <<"\t4. Office\n\t5. Shop\n\t6. Mall\n"
+            <<"Industrial Buildings:\n"
+            <<"\t7. Factory\n\t8. Warehouse\n\t9. Plant\n"
+            <<"Landmark Buildings:\n"
+            <<"\t10. Park\n\t11. Cultural Centre\n\t12. Park\n"
+            <<"====================================\n";
+        cin>>type;
+        build(type);
+        // TODO: integrate Building Manager here
         // tell Building Manager to add a building
     } else if (choice == 2){
         this->c->implementPolicy();
     }
     return false;
+}
+
+void build(int type){
+    if (type <= 3){
+        // build residential building with parameter type = type
+    } else if (type <= 6){
+        // build commercial building with type = (type+1)%4
+    } else if (type <= 9){
+        // build industrial building with type = (type+2)%4
+    } else if (type <= 12){
+        //build landmark with type = (type+3)%4
+    } else {
+        std::cout<<"=> Invalid input\n";
+    }
 }
