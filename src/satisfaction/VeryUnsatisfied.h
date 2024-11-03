66efbc686bd1a87d23f08490f2cecfa560ec5ccc
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-04 01:52:47 +0200

    SatisfactionStates Dox Comments

--- src/satisfaction/VeryUnsatisfied.h
+++ src/satisfaction/VeryUnsatisfied.h
@@ -3,11 +3,33 @@
 
 #include "SatisfactionState.h"
 
+/**
+ * @class VeryUnsatisfied
+ * @brief Represents a VeryUnsatisfied satisfaction state within a city.
+ *
+ * The VeryUnsatisfied class provides the implementation of a VeryUnsatisfied satisfaction state,
+ * with operations to adjust the state as necessary.
+ */
 class VeryUnsatisfied : public SatisfactionState {
     public:
+        /**
+         * @brief Constructs a VeryUnsatisfied satisfaction state.
+         */
         VeryUnsatisfied();
+        /**
+         * @brief Retrieves the current satisfaction level as a string.
+         * @return "VeryUnsatisfied" satisfaction level as a string.
+         */
         std::string getSatisfactionLevel();
+        /**
+         * @brief Increases the satisfaction level of the city from VeryUnsatisfied to Unsatisfied.
+         * @param c Pointer to the City object whose satisfaction is to be increased.
+         */
         void incSatisfactionState(City* c);
+        /**
+         * @brief Decreases the satisfaction level of the city from VeryUnsatisfied to VeryUnsatisfied. (Lowest SatisfactionState)
+         * @param c Pointer to the City object whose satisfaction is to be decreased.
+         */
         void decSatisfactionState(City* c);
 };
 
