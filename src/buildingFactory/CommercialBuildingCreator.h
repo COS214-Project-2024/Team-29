cb74b93c5bfc71df69b92f3e832deb22ea9d3089
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-04 01:52:25 +0200

    Factory creators Dox Comments

--- src/buildingFactory/CommercialBuildingCreator.h
+++ src/buildingFactory/CommercialBuildingCreator.h
@@ -5,9 +5,33 @@
 #include "../buildingComposite/Building.h"
 
 #include <iostream>
+/**
+ * @class CommercialBuildingCreator
+ * @brief A concrete factory method for creating buildings.
+ *
+ * The CommercialBuildingCreator class provides one of the concrete classes for 
+ * the abstract factory class, BuidlingCreator, and is responsible for creating
+ * Building objects of type Commercial
+ */
 
 class CommercialBuildingCreator : public BuildingCreator {
     public:
+        /**
+         * @brief Creates a building object of type Commercial and returns it.
+         * 
+         * createBulding() takes in a int paramenter that defines the type of
+         * commercial building
+         * - Office : bName == 1
+         * - Shop : bName == 2
+         * - Mall : bName == 3<br><br>
+         * (returns nullptr if bName<1 or bName>3)<br>
+         * returns a Building pointer to the object (Building*).
+         *
+         * @param [in] bName int that determines the type of Commercial building created
+         * 
+         * 
+         * @return Building* to Building object created
+         */
         Building* createBuilding(int bName);
 };
 
--- src/buildingFactory/IndustrialBuildingCreator.h
+++ src/buildingFactory/IndustrialBuildingCreator.h
@@ -5,10 +5,32 @@
 #include "../buildingComposite/Building.h"
 
 #include <iostream>
-
+/**
+ * @class IndustrialBuildingCreator
+ * @brief A concrete factory method for creating buildings.
+ *
+ * The IndustrialBuildingCreator class provides one of the concrete classes for 
+ * the abstract factory class, BuidlingCreator, and is responsible for creating
+ * Building objects of type Industrial
+ */
 class IndustrialBuildingCreator : public BuildingCreator
 {
     public:
+        /**
+         * @brief Creates a building object of type Industrial and returns it.
+         * 
+         * createBulding() takes in a int paramenter that defines the type of
+         * industrial building
+         * - Factory : bName == 1
+         * - Warehouse : bName == 2
+         * - Plant : bName == 3<br><br>
+         * (returns nullptr if bName<1 or bName>3)<br>
+         * returns a Building pointer to the object (Building*).
+         *
+         * @param [in] bName int that determines the type of Industrial building created
+         * 
+         * @return Building* to Building object created
+         */
         Building* createBuilding(int bName);
 };
 
