3dc0d3b27d2bbd7451bd2641c1b22e238bc22907
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-04 01:52:47 +0200

    SatisfactionStates Dox Comments

--- src/satisfaction/Satisfied.h
+++ src/satisfaction/Satisfied.h
@@ -3,11 +3,33 @@
 
 #include "SatisfactionState.h"
 
+/**
+ * @class Satisfied
+ * @brief Represents a Satisfied satisfaction state within a city.
+ *
+ * The Satisfied class provides the implementation of a Satisfied satisfaction state,
+ * with operations to adjust the state as necessary.
+ */
 class Satisfied : public SatisfactionState {
     public:
+        /**
+         * @brief Constructs a Satisfied satisfaction state.
+         */
         Satisfied();
+        /**
+         * @brief Retrieves the current satisfaction level as a string.
+         * @return "Satisfied" satisfaction level as a string.
+         */
         std::string getSatisfactionLevel();
+        /**
+         * @brief Increases the satisfaction level of the city from Satisfied to VerySatisfied.
+         * @param c Pointer to the City object whose satisfaction is to be increased.
+         */
         void incSatisfactionState(City* c);
+        /**
+         * @brief Decreases the satisfaction level of the city from Satisfied to Neutral.
+         * @param c Pointer to the City object whose satisfaction is to be decreased.
+         */
         void decSatisfactionState(City* c);
 };
 
--- src/satisfaction/Unsatisfied.h
+++ src/satisfaction/Unsatisfied.h
@@ -3,11 +3,33 @@
 
 #include "SatisfactionState.h"
 
+/**
+ * @class Unsatisfied
+ * @brief Represents a Unsatisfied satisfaction state within a city.
+ *
+ * The Unsatisfied class provides the implementation of a Unsatisfied satisfaction state,
+ * with operations to adjust the state as necessary.
+ */
 class Unsatisfied : public SatisfactionState {
     public:
+        /**
+         * @brief Constructs a Unsatisfied satisfaction state.
+         */
         Unsatisfied();
+        /**
+         * @brief Retrieves the current satisfaction level as a string.
+         * @return "Unsatisfied" satisfaction level as a string.
+         */
         std::string getSatisfactionLevel();
+        /**
+         * @brief Increases the satisfaction level of the city from Unsatisfied to Neutral.
+         * @param c Pointer to the City object whose satisfaction is to be increased.
+         */
         void incSatisfactionState(City* c);
+        /**
+         * @brief Decreases the satisfaction level of the city from Unsatisfied to VeryUnsatisfied.
+         * @param c Pointer to the City object whose satisfaction is to be decreased.
+         */
         void decSatisfactionState(City* c);
 };
 
