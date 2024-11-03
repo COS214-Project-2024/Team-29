ce42e3d9035af631dff3f65b14006a2d485424b9
DylanHebron <dylanellishebron@gmail.com>
2024-11-03 20:30:54 +0200

    "Changed naming of creator type from type to bName, to distinguish between bType(e.g. Residential) and bName(e.g.House"

--- src/buildingFactory/ResidentialBuildingCreator.h
+++ src/buildingFactory/ResidentialBuildingCreator.h
@@ -9,11 +9,7 @@
 class ResidentialBuildingCreator : public BuildingCreator
 {
     public:
-<<<<<<< HEAD
         Building* createBuilding(int bName);
-=======
-        Building* createBuilding(int type);
->>>>>>> develop
 };
 
 #endif
