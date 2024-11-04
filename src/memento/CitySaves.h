352fd60e501678831806f9b0587457003adaf6f5
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-04 20:34:24 +0200

    Memento Dox Comments

--- src/memento/CitySaves.h
+++ src/memento/CitySaves.h
@@ -3,14 +3,35 @@
 
 #include "CityMemento.h"
 
+/**
+ * @class CitySaves
+ * @brief Manages saving and loading of city states using CityMemento objects.
+ *
+ * The CitySaves class provides methods to add, remove, and retrieve saved city states.
+ */
 class CitySaves {
-    public:
-        addSave(CityMemento* m);
-        removeSave();
-        CityMemento* getSave();
-        ~CityMemento();
     private:
-        CityMemento* save;
+        CityMemento* save;  /**< Pointer to the saved city state. */
+
+    public:
+        /**
+         * @brief Adds a save state to the collection.
+         * @param m Pointer to the CityMemento representing the city state to be saved.
+         */
+        addSave(CityMemento* m);
+        /**
+         * @brief Removes the last saved state from the collection.
+         */
+        removeSave();
+        /**
+         * @brief Retrieves the most recent save state.
+         * @return Pointer to the most recent CityMemento instance.
+         */
+        CityMemento* getSave();
+        /**
+         * @brief Destructor for CitySaves, cleaning up resources.
+         */
+        ~CityMemento();
 };
 
 #endif
