e8f5475749a606184d4e485056805584da681743
Tom Schulz <schulztom02@gmail.com>
2024-11-04 08:33:29 +0200

    Added Interface to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+#include "managers/Interface.h"
+
 int main()
 {
+    Interface* gameInterface = new Interface();
+
+    //start simulation
+    gameInterface->startSimulation();
+    //startSimulation has its own game over conditions
+    //game will be over after startSimulation is done running
+
+    delete gameInterface;
     return 0;
 }
