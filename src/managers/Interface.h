6d7230abf3a4e4263e9cdcf490adf9df0f6959f6
RaphaelRato <raphaelrato2002@gmail.com>
2024-11-04 20:33:40 +0200

    Managers Dox Comments

--- src/managers/Interface.h
+++ src/managers/Interface.h
@@ -3,17 +3,40 @@
 
 #include "City.h"
 
+/**
+ * @class Interface
+ * @brief Manages the user interface and simulation control.
+ *
+ * The Interface class provides functions to initiate the simulation, handle user choices, and manage building operations.
+ */
 class Interface {
     private:
-        City* c;
-        int cycle;
+        City* c;    /**< Pointer to the City instance. */
+        int cycle;  /**< Simulation cycle counter. */
     
     public:
-        /// @brief default constructor for Interface
+        /**
+         * @brief Default constructor for Interface.
+         */
         Interface();
+        /**
+         * @brief Starts the simulation process.
+         */
         void startSimulation();
+        /**
+         * @brief Handles user choices during simulation.
+         * @param choice User-selected choice.
+         * @return True if the choice is successfully handled, false otherwise.
+         */
         bool handleChoice(int choice);
+        /**
+         * @brief Initiates building construction within the simulation.
+         * @param type Type of building to construct.
+         */
         void build(int type);
+        /**
+         * @brief Destructor for Interface.
+         */
         ~Interface();
 };
 
